c6f0c1ab2ef08e6564078d8b7a38f0b2b1d07cf3
lokhansn@gmail.com <lokhansn@gmail.com>
2021-07-16 16:34:24 +0530

    adding localhost config for order and inventory service

--- src/main/java/com/sgl/gateway/config/LocalHostRouteConfig.java
+++ src/main/java/com/sgl/gateway/config/LocalHostRouteConfig.java
@@ -13,6 +13,10 @@ public class LocalHostRouteConfig {
         return builder.routes()
                 .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
                         .uri("http://localhost:8080"))
+                .route(r -> r.path("/api/v1/customers/**")
+                        .uri("http://localhost:8081"))
+                .route(r -> r.path("/api/v1/beer/*/inventory")
+                        .uri("http://localhost:8082"))
                 .build();
     }
 
